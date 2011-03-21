a55abbe5e4708ef86d815d45996b615f62a800dd
Atif Raza <atif56@gmail.com>
2011-03-20 17:39:25 -0700

    added helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+def title  # function defined for use in views
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
